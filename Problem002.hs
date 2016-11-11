b28988bb829d77e3e8c9eceb6a66cb81dfcd6219
Arjun Shanker <me@shankera.com>
2016-11-10 19:47:44 -0500

    Problem 001 - Haskell

--- Problem002.hs
+++
@@ -1,7 +0,0 @@
--- Version 1 --
-main = print (sum (filter (\n -> n `mod` 2 == 0) (fibTill list)))
-list = [1,2]
-fibTill::[Int]->[Int]
-fibTill list1
-    | (last list1) > 3999999 = (init list1)
-    | otherwise = fibTill (list1 ++ [(last list1) + (last (init list1))])
